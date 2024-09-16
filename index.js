553160b267975343077bcb936a463f43a70262d6
Baas Zunnaiyyer <baaszunnaiyyer@gmail.com>
2024-09-16 22:57:59 +0500

    CRUD Test part 1

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const app = express();
+
+app.get('/' ,(req,res)=>{
+    res.status(200).send("Hello world Form Node API")
+})
+
+mongoose.connect("mongodb+srv://baaszunnaiyyer:TgfIhEGuNmo3iV0M@crudtestapi.dzs51.mongodb.net/CRUDTestAPI?retryWrites=true&w=majority&appName=CRUDTestAPI")
+.then(()=>{
+    console.log("Connected To the Database");
+    app.listen(3000, ()=>{
+        console.log("Server has started at Port 3000");
+        
+    })
+    
+}).catch(()=>{
+    console.log("Connection failed");
+    
+})
